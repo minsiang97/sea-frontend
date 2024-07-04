f3f30106cd339c794874a67b9bf21acb6362d70c
Ong Min Siang <ongminsiang@gmail.com>
2024-07-04 19:36:36 +0800

    initial-setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['./src'],
+        alias: {
+          '@redux': './src/redux',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
